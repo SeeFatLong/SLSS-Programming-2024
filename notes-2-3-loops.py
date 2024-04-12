9dc9bc18c5d16169cde21affaa50861cf3b80161
Bryant <bryantchan0314@gmail.com>
2024-04-12 10:00:35 -0700

    Unit mid 2 activity

---
+++ notes-2-3-loops.py
@@ -0,0 +1,27 @@
+# 
+
+# Print 'something' 10 times
+for _ in 
+
+# Print out every item in muy grocery list
+grocery_list = [
+    "dishwasher tabs",
+    "aluminium foil",
+    "blueberry muffins",
+    "RTX 4090 Super",
+]
+
+for item in grocery_list:
+    print("___")
+    print(f"* {item}")
+
+    if item == "blueberry muffins":
+        break
+    
+# Count from 0 to 9
+for i in range(10):
+    # Modulo
+    if i % 2 == 0:
+        print (f"{i} is an even number")
+
+    # rewrite the above for loop 
